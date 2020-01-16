d08d05a389666f5687a67bf11591059de2f46f70
tylerbadger23 <31893030+tylerbadger23@users.noreply.github.com>
2020-01-15 18:00:42 -0700

    Fixed oversight with usernames
    Before usernames where created by the first part of an email. But when a user created an account with the same email part, it was possinle to have the same username shared between two users. So users have to select a username when they sign up.

--- OnlineFileEditor/form_handlers/register_user.php
+++ OnlineFileEditor/form_handlers/register_user.php
@@ -7,7 +7,7 @@ $email = mysqli_real_escape_string($conn, $_POST['reg_email']);
 $email = strtolower($email);
 $password = mysqli_real_escape_string($conn, $_POST['reg_pw']);
 $password2 = mysqli_real_escape_string($conn, $_POST['reg_pw2']);
-$username = mysqli_real_escape_string($conn, $_POST['username']);
+$username = mysqli_real_escape_string($conn, $_POST['reg_username']);
 
 //PASSWORD CHECKS
 if($password !== $password2) {
@@ -26,8 +26,8 @@ if (strlen($email) < 5){
     exit;
 }
 
-if (strlen($username) < 5 || strlen($username) > 16){
-    $errorMsg = "Your username must be between 5 and 16 characters";
+if (strlen($username) < 4 || strlen($username) > 16){
+    $errorMsg = "Your username must be between 4 and 16 characters";
     header("Location: ../register.php?error=$errorMsg");
     exit;
 }
@@ -38,19 +38,26 @@ if (strlen($username) < 5 || strlen($username) > 16){
 $hashed_password = md5($password);
 //filter data BEFORE PUBLISHING
 
-$selectQuery = "SELECT * FROM users WHERE email='$email'";
+$selectQuery = "SELECT id FROM users WHERE email='$email'";
 $results = mysqli_query($conn, $selectQuery);
 
+$username_check_query = "SELECT id FROM users WHERE username='$username'";
+$username_query_results = mysqli_query($conn, $username_check_query);
+
 if(mysqli_num_rows($results) > 0) {
-    $errorMsg = "User already exists. Plese try logging in instread. ";
+    $errorMsg = "This email is already taken. Please try logging in instread.";
     header("Location: ../register.php?error=$errorMsg");
-    echo("Phase 2 Completed");
+    exit;
+} else if(mysqli_num_rows($username_query_results) > 0) {
+    $errorMsg = "username is  already taken. Please choose another one";
+    header("Location: ../register.php?error=$errorMsg");
     exit;
 } else {
     $query = "INSERT INTO `users` (`id`, `email`, `password`,`username`) VALUES (NULL, '$email', '$hashed_password','$username')";
     if(mysqli_query($conn, $query)){
-        $errorMsg = "Successfully Created New Account";
-        header("Location: ../index.php?s=$errorMsg");
+        $successMsg = "Successfully Created New Account";
+        $_SESSION['username'] = $username;
+        header("Location: ../index.php?s=$successMsg");
         exit;
     }
     echo("Phase 3 Completed");
